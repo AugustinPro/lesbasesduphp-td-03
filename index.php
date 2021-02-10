358217fa97cfb227ad0be1c9b3c1ac086b2c2c9c
Augustin Petre <apetre.pro@gmail.com>
2021-02-10 10:31:48 +0100

    done

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+
+date_default_timezone_set('Europe/Paris');
+
+?>
+
+
 <!doctype html>
 <html lang="fr">
 
@@ -9,7 +17,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Date</title </head>
+    <title>Date</title>
 </head>
 
 <body class=" bg-light">
@@ -21,57 +29,89 @@
                 <h2>Date du jour</h2>
 
                 <!-- 1. date du jour -->
-                <p class="text-muted">Nous somme le <?php // votre code ici ?></p>
+                <p class="text-muted">Nous somme le <?php echo date('d-m-Y'); ?></p>
 
                 <!-- 2. date du jour textuelle -->
-                <p class="text-muted">Nous somme <?php // votre code ici ?></p>
+                <p class="text-muted">Nous somme <?php echo date('jS F Y'); ?></p>
 
                 <!-- 3. L'heure courante -->
                 <!-- Utilisation de la balise time (datetime prend date ISO ) -->
-                <p class="text-muted">Il est <time datetime=""><?php // votre code ici ?></time></p>
+                <p class="text-muted">Il est <time datetime="<?= date('H:i'); ?>"><?php echo date('H:i') ?></time></p>
 
                 <!-- 4. date en français -->
                 
                 <!-- Définir la configuration locale par défaut au fr  -->
-                <p class="text-muted">Nous somme <?php // votre code ici ?></p>
+                <p class="text-muted">Nous somme <?php 
+                    setlocale(LC_TIME, 'fr_FR');
+                    echo utf8_encode(strftime("%A %e %B %Y"));
+                ?>
+                </p>
                 <br>
                 
                 <!-- 5. Timestamp courant -->
                 <h2>Timestamp courant</h2>
-                <p class="text-muted">Timestamp courant <?php // votre code ici ?></p>
+                <p class="text-muted">Timestamp courant <?php 
+                    $date = date_create();
+                    $show = date_timestamp_get($date);
+                    echo $show;
+                ?>
+                </p>
                
                 <!-- 6. Calcul de date -->
                 <h2>Calcul de date</h2>
                 <!-- Faites vos calculs ici -->
-                <p class="text-muted">Nombre de jour dépuis le debut des cours <?php // votre code ici ?></p>
+                <p class="text-muted">Nombre de jour dépuis le debut des cours <?php 
+                    
+                ?>
+                </p>
                
                 <!-- 7. J + 25 -->
                
                <!-- Faites vos calculs ici -->
-                <p class="text-muted">La date J + 25 est : <?php // votre code ici ?></p>
+                <p class="text-muted">La date J + 25 est : <?php echo date('jS F Y', strtotime('+25 days')); ?></p>
                 
                 <!-- 8. J - 15 -->
                <!-- Faites vos calculs ici -->
-                <p class="text-muted">La date J - 15 est : <?php // votre code ici ?></p>
+                <p class="text-muted">La date J - 15 est : <?php echo date('jS F Y', strtotime('-15 days')); ?></p>
                 <br>
                 
                 <!-- 9. Nb de jour dans le mois -->
                 <h2>Nombre de jour dans le mois</h2>
-                <p class="text-muted">Nombre de jour : <?php // votre code ici ?></p>
+                <p class="text-muted">Nombre de jour : <?php 
+                    $days = date("m");
+                    $year = date('Y');
+                    echo $number = cal_days_in_month(CAL_GREGORIAN, $days, $year);
+                ?>
+                </p>
                 
                 <!-- 10. Timestamp Epoch -->
                 <h2>Timestamp du 01/01/1970 00:00</h2>
-                <p class="text-muted">Nombre de jour : <?php // votre code ici ?></p>
+                <p class="text-muted">Nombre de jour : <?php 
+                    $date2 = new DateTime("1970-01-01 00:00");
+                    echo $date2->format("U");
+                ?>
+                </p>
                 <hr>
                
                 <!-- 11 date du timestamp -->
                 <h2>Date du timestamp 1601532000</h2>
-                <p class="text-muted">Date correspondante est : <?php // votre code ici ?></p>
+                <p class="text-muted">Date correspondante est : <?php
+                    $date3 = date('d-m-Y H:i:s', 1601532000);
+                    echo $date3;
+                ?>
+                </p>
                 
                 <!-- 12 calcul âge -->
                 <h2>Calcul de l'âge </h2>
                  <!-- Faites vos calculs ici -->
-                <p class="text-muted">L'âge de personne née 1 avril 1978 est : <?php // votre code ici ?> .</p>
+                <p class="text-muted">L'âge de personne née 1 avril 1978 est : <?php
+                    $birthdate = new DateTime("04-01-1978");
+                    $currdate = new DateTime();
+                    $age = $currdate->diff($birthdate);
+                    echo $age->y;
+
+                ?>
+                 .</p>
             </div>
         </div>
 
